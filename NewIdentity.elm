24ec4f4dcd86af2e95265645c4684468549f16ca
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2016-02-20 15:23:30 -0500

    Initial Commit
    - Static UI
    - CSS Reset
    - Custom CSS

---
+++ NewIdentity.elm
@@ -0,0 +1,39 @@
+module NewIdentity where
+
+import Html exposing (..)
+import Html.Attributes exposing (class, src)
+
+type alias Address =
+  { street: String
+  , city: String
+  , state: String
+  , zip: String
+  }
+
+
+type alias Model =
+  { firstName: String
+  , lastName: String
+  , adress: Address
+  , email: String
+  , photo: String
+  }
+
+
+main : Html
+main  =
+    div
+      [ class "identity-card" ]
+      [ div
+          [ class "identity-card--info" ]
+          [ h4 [ class "info--name" ] [ text "John Doe" ]
+          , p [] [ text "123 Street" ]
+          , p [] [ text "Montreal" ]
+          , p [] [ text "Quebec" ]
+          , p [] [ text "H0H 0H0" ]
+          , p [] [ text "john.doe@webhost.com" ]
+          ]
+      , div
+          [ class "identity-card--picture" ]
+          [ img [ src "https://randomuser.me/api/portraits/med/men/69.jpg" ] [] ]
+      ]
