592c2912fc24ff477ae4e5970df131031859041e
Ny Fanilo Andrianjafy <ny.fanilo@gmail.com>
2016-02-20 15:35:48 -0500

    Added the button

--- NewIdentity.elm
+++ NewIdentity.elm
@@ -21,19 +21,23 @@ type alias Model =
 
 
 main : Html
-main  =
+main =
     div
-      [ class "identity-card" ]
+      [ class "app-container"]
       [ div
-          [ class "identity-card--info" ]
-          [ h4 [ class "info--name" ] [ text "John Doe" ]
-          , p [] [ text "123 Street" ]
-          , p [] [ text "Montreal" ]
-          , p [] [ text "Quebec" ]
-          , p [] [ text "H0H 0H0" ]
-          , p [] [ text "john.doe@webhost.com" ]
+          [ class "identity-card" ]
+          [ div
+              [ class "identity-card--info" ]
+              [ h4 [ class "info--name" ] [ text "John Doe" ]
+              , p [] [ text "123 Street" ]
+              , p [] [ text "Montreal" ]
+              , p [] [ text "Quebec" ]
+              , p [] [ text "H0H 0H0" ]
+              , p [] [ text "john.doe@webhost.com" ]
+              ]
+          , div
+              [ class "identity-card--picture" ]
+              [ img [ src "https://randomuser.me/api/portraits/med/men/69.jpg" ] [] ]
           ]
-      , div
-          [ class "identity-card--picture" ]
-          [ img [ src "https://randomuser.me/api/portraits/med/men/69.jpg" ] [] ]
+      , button [ class "button" ] [ text "Get New Identity" ]
       ]
